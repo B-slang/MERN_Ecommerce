6dd71828dbcf464c5f90a8f9f8a3b343a796360e
B-slang <entr.brijesh@gmail.com>
2021-04-20 10:10:40 +0530

    models created

---
+++ projbackend/models/product.js
@@ -0,0 +1,45 @@
+const { isValidObjectId, model } = require('mongoose');
+const mongoose = require('moongoose');
+const {ObjectId} = mongoose.Schema;
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32,
+    },
+    description:{
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 1500
+    },
+    price:{
+        type: Number,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    category:{
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    },
+
+    stock:{
+        type: Number
+    },
+    sold:{
+        type: Number,
+        default: 0
+
+    },
+    photo:{
+        data: Buffer,
+        ContentType: String
+    }
+
+},{timestamps: true});
+
+model.exports = mongoose.model("Product", productSchema);
