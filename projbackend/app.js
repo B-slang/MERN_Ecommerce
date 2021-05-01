d3f832d927504c834953e050fc341f4aa775177a
B-slang <entr.brijesh@gmail.com>
2021-05-01 20:50:47 +0530

    Checking routes

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
+
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+// const { cookie } = require("express-validor");
+const authRoutes = require("./routes/authentication");
 
+//DB CONNECT
 mongoose.connect(process.env.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -12,9 +19,18 @@ mongoose.connect(process.env.DB, {
     console.log("DB CONNECTED")
 });
 
+// MIDDLEWARE
+app.use(bodyParser.json()); //body parser application json.
+app.use(cookieParser());
+app.use(cors());
+
+//ROUTES
+app.use("/api", authRoutes);
+
+// PORT
 const port = 8000;
 
-
+// STARTING A SERVER
 app.listen(port, () => {
     console.log(`app is running at ${port}`)
 
