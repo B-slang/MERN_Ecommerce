92dc71d17f836e8951e4ab3ad5b128a8bb9fad3b
B-slang <entr.brijesh@gmail.com>
2021-08-13 18:42:24 +0530

    used populate method here for collection

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,24 +1,70 @@
+const user = require("../models/user");
 const User = require("../models/user");
-
-
+const Order = require("../models/order")
 
 // works with param thats why id is here cb
-exports.getUserById = (req, res, next, id)=> {
-    User.findById(id).exec((err, user) =>{
-        if (err || !user){
-            return res.status(400).json({
-                error: "no user in db"
-            });
-        }
-        req.profile = user;
-        next();
-    });        
-
+exports.getUserById = (req, res, next, id) => {
+  User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "no user in db",
+      });
+    }
+    req.profile = user;
+    next();
+  });
 };
-
 
 exports.getUser = (req, res) => {
   req.profile.salt = undefined;
   req.profile.encry_password = undefined;
   return res.json(req.profile);
+};
+
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile.id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "you are not authorized to update this user",
+        });
+      }
+      user.salt = undefined;
+      user.encry_password = undefined;
+      res.json(user);
+    }
+  );
+};
+
+
+// exports.userPurchaseList = (req, res) =>{
+//   Order.find({user: req.profile._id})
+//   .populate("user","_id name")
+//   .exec((err, order) =>{
+//     if (err){
+//       return res.status(400).json({
+//         error: "no order in this acc"
+//       });
+//     }
+//     return res.json(order);
+//   });
+
+// };
+
+
+
+exports.userPurchaseList = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .populate("user", "_id name")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "No Order in this account"
+        });
+      }
+      return res.json(order);
+    });
 };
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser } = require("../controllers/user");
+const { getUserById, getUser, updateUser, userPurchaseList } = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/authentication");
 
 router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser )
+
+
+//populate
+// router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList ) // dnt know why it causes errr
+router.get(
+    "/orders/user/:userId",
+    isSignedIn,
+    isAuthenticated,
+    userPurchaseList
+  );
+  
+
+
+
 
 module.exports = router;
