7392fe66c25ff4ec283d663fe74626f7b831d031
B-slang <entr.brijesh@gmail.com>
2021-08-18 12:57:40 +0530

    injected backend into frontnd n checking base comp

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Base = ({
+    title= "My Title",
+    description= "My description",
+    className= "bg-dark text-white p-4",
+    children 
+}) => (
+  <div>
+    <div className="container fluid">
+      <div className="jumbotron bg-dark text-white text-center">
+                <h2 className="display-4">{title}</h2>
+                <p className="lead">{ description}</p>
+      </div>
+            <div className={className}>{ children}</div>
+    </div>
+    <footer className="footer bg-dark mt-auto py-3">
+      <div className="container-fluid bg-success text-white text-center">
+        <h4>If you got any q feel free to reach out</h4>
+        <button className="btn btn-warning btn-lg">Contact us</button>
+      </div>
+      <div className="container">
+        <span className="text-muted">An Amazing place to buy T-shirts</span>
+      </div>
+    </footer>
+  </div>
+);
+
+export default Base;
---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,31 @@
+import React from "react"
+import "../styles.css";
+import { API } from "../backend";
+import Base from "../core/Base"
+
+export default function Home() {
+    console.log("API IS" , API);
+    return (
+        <Base title="Home page">
+            {/* <h1>hellow</h1> */}
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">
+                        Test
+                    </button>
+                </div>
+                <div className="col-4">
+                <button className="btn btn-success">
+                        Test
+                    </button>
+                </div>
+                <div className="col-4">
+                <button className="btn btn-success">
+                        Test
+                    </button>
+                </div>
+
+            </div>
+        </Base>
+    )
+}
