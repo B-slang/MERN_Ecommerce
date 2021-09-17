47e0d42986c0ed5eb2dde1ae04d514151942dcdd
B-slang <entr.brijesh@gmail.com>
2021-09-17 12:43:06 +0530

    cart final amount issue solved

--- projfrontend/src/core/Cart.js
+++ projfrontend/src/core/Cart.js
@@ -4,55 +4,51 @@ import { API } from "../backend";
 import Base from "./Base";
 import Card from "./Card";
 import { loadCart } from "./helper/cartHelper";
-
+import StripeCheckout from "./StripeChekout";
 
 const Cart = () => {
- 
-    const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [reload, setReload] = useState(false);
 
-    useEffect(() => {
-        setProducts(loadCart())
-    }, [])
+  useEffect(() => {
+    setProducts(loadCart());
+  }, [reload]);
 
+  const loadAllProducts = () => {
+    return (
+      <div>
+        <h2>This section is to load products</h2>
+        {products.map((product, index) => (
+          <Card
+            key={index}
+            product={product}
+            removeFromCart={true}
+            addtoCart={false}
+            setReload={setReload}
+            reload={reload}
+          />
+        ))}
+      </div>
+    );
+  };
+  const loadCheckout = () => {
+    return (
+      <div>
+        <h2>This section for checkout</h2>
+      </div>
+    );
+  };
 
-
-    const loadAllProducts = () => {
-        return (
-            <div>
-                <h2>This section is to load products</h2>
-                {products.map((product, index) => (
-                    <Card
-                        key={index}
-                        product={product}
-                        removeFromCart={true}
-                        addtoCart = {false}
-    
-                    />
-                ))}
-            </div>
-        )
-    }
-
-    const loadCheckout = () => {
-        return (
-            <div>
-                <h2>This section is for checkout</h2>
-            </div>
-        )
-    }
-
-
- 
   return (
     <Base title="Cart Page" description="Ready to checkout">
       <div className="row text-center">
-              <div className="col-6">{ loadAllProducts()}</div>
-              <div className="col-6">{ loadCheckout()}</div>
-              
-        
+        <div className="col-6">{loadAllProducts()}</div>
+        <div className="col-6">
+          <StripeCheckout products={products} setReload={setReload} />
+        </div>
       </div>
     </Base>
   );
-}
+};
 
 export default Cart;
