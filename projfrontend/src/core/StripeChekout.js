330fdb8f03b1e486b6309d91c864121f5e22acca
B-slang <entr.brijesh@gmail.com>
2021-09-22 15:45:43 +0530

    cart bugs need to fix overall done

--- projfrontend/src/core/StripeChekout.js
+++ projfrontend/src/core/StripeChekout.js
@@ -2,17 +2,20 @@ import React, { useState, useEffect } from "react";
 import { isAutheticated } from "../auth/helper";
 import { cartEmpty, loadCart } from "./helper/cartHelper";
 import { Link } from "react-router-dom";
+import StripeCheckoutButton from "react-stripe-checkout";
+import API from "../backend";
+import createOrder from "./helper/OderHelper"
 
 const StripeCheckout = ({
   products,
-  setReload = f => f,
-  reload = undefined
+  setReload = (f) => f,
+  reload = undefined,
 }) => {
   const [data, setData] = useState({
     loading: false,
     success: false,
     error: "",
-    address: ""
+    address: "",
   });
 
   const token = isAutheticated() && isAutheticated().token;
@@ -20,15 +23,42 @@ const StripeCheckout = ({
 
   const getFinalAmount = () => {
     let amount = 0;
-    products.map(p => {
+    products.map((p) => {
       amount = amount + p.price;
     });
     return amount;
   };
 
+  const makePayment = (token) => {
+    const body = {
+      token,
+      products,
+    };
+    const headers = {
+      content: "application/json",
+    };
+    return fetch(`${API}/stripepayment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    }).then(response => {
+      console.log(response)
+    }).catch(err => console.log(err))
+  };
+
   const showStripeButton = () => {
     return isAutheticated() ? (
-      <button className="btn btn-success">Pay with stripe</button>
+      <StripeCheckoutButton
+        stripeKey=""
+        token={makePayment()}
+        amount={getFinalAmount() * 100}
+        name="Buy Tshirts"
+        shippingAddress=""
+        billingAddress=""
+        currency="INR"
+      >
+        <button className="btn btn-success">Pay with stripe</button>
+      </StripeCheckoutButton>
     ) : (
       <Link to="/signin">
         <button className="btn btn-warning">Signin</button>
